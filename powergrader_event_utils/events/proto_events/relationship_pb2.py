9a69257b3c50354ed2f4ec992abcdc1d74cde2f3
Dallin Hutchison <dallinhutchison@gmail.com>
2023-10-16 16:06:52 -0600

    added a new event to register public uuids to private uuids

--- powergrader_event_utils/events/proto_events/relationship_pb2.py
+++ powergrader_event_utils/events/proto_events/relationship_pb2.py
@@ -13,21 +13,21 @@ _sym_db = _symbol_database.Default()
 
 
 
-DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x12relationship.proto\x12\x0crelationship\"?\n\x12RegisterPublicUuid\x12\x14\n\x0cprivate_uuid\x18\x01 \x01(\t\x12\x13\n\x0bpublic_uuid\x18\x02 \x01(\t\"A\n\x16\x41ssignmentAddedToClass\x12\x15\n\rassignment_id\x18\x01 \x01(\t\x12\x10\n\x08\x63lass_id\x18\x02 \x01(\t\"E\n\x1a\x41ssignmentRemovedFromClass\x12\x15\n\rassignment_id\x18\x01 \x01(\t\x12\x10\n\x08\x63lass_id\x18\x02 \x01(\t\";\n\x13StudentAddedToClass\x12\x12\n\nstudent_id\x18\x01 \x01(\t\x12\x10\n\x08\x63lass_id\x18\x02 \x01(\t\"?\n\x17StudentRemovedFromClass\x12\x12\n\nstudent_id\x18\x01 \x01(\t\x12\x10\n\x08\x63lass_id\x18\x02 \x01(\tb\x06proto3')
+DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x12relationship.proto\x12\x0crelationship\"A\n\x14PublicUuidRegistered\x12\x14\n\x0cprivate_uuid\x18\x01 \x01(\t\x12\x13\n\x0bpublic_uuid\x18\x02 \x01(\t\"A\n\x16\x41ssignmentAddedToClass\x12\x15\n\rassignment_id\x18\x01 \x01(\t\x12\x10\n\x08\x63lass_id\x18\x02 \x01(\t\"E\n\x1a\x41ssignmentRemovedFromClass\x12\x15\n\rassignment_id\x18\x01 \x01(\t\x12\x10\n\x08\x63lass_id\x18\x02 \x01(\t\";\n\x13StudentAddedToClass\x12\x12\n\nstudent_id\x18\x01 \x01(\t\x12\x10\n\x08\x63lass_id\x18\x02 \x01(\t\"?\n\x17StudentRemovedFromClass\x12\x12\n\nstudent_id\x18\x01 \x01(\t\x12\x10\n\x08\x63lass_id\x18\x02 \x01(\tb\x06proto3')
 
 _builder.BuildMessageAndEnumDescriptors(DESCRIPTOR, globals())
 _builder.BuildTopDescriptorsAndMessages(DESCRIPTOR, 'relationship_pb2', globals())
 if _descriptor._USE_C_DESCRIPTORS == False:
 
   DESCRIPTOR._options = None
-  _REGISTERPUBLICUUID._serialized_start=36
-  _REGISTERPUBLICUUID._serialized_end=99
-  _ASSIGNMENTADDEDTOCLASS._serialized_start=101
-  _ASSIGNMENTADDEDTOCLASS._serialized_end=166
-  _ASSIGNMENTREMOVEDFROMCLASS._serialized_start=168
-  _ASSIGNMENTREMOVEDFROMCLASS._serialized_end=237
-  _STUDENTADDEDTOCLASS._serialized_start=239
-  _STUDENTADDEDTOCLASS._serialized_end=298
-  _STUDENTREMOVEDFROMCLASS._serialized_start=300
-  _STUDENTREMOVEDFROMCLASS._serialized_end=363
+  _PUBLICUUIDREGISTERED._serialized_start=36
+  _PUBLICUUIDREGISTERED._serialized_end=101
+  _ASSIGNMENTADDEDTOCLASS._serialized_start=103
+  _ASSIGNMENTADDEDTOCLASS._serialized_end=168
+  _ASSIGNMENTREMOVEDFROMCLASS._serialized_start=170
+  _ASSIGNMENTREMOVEDFROMCLASS._serialized_end=239
+  _STUDENTADDEDTOCLASS._serialized_start=241
+  _STUDENTADDEDTOCLASS._serialized_end=300
+  _STUDENTREMOVEDFROMCLASS._serialized_start=302
+  _STUDENTREMOVEDFROMCLASS._serialized_end=365
 # @@protoc_insertion_point(module_scope)
