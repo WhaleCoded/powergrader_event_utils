4d75b8ac3ef63b0da749995efda010a5eb4f2f7a
Dallin Hutchison <dallinhutchison@gmail.com>
2023-11-29 12:40:32 -0700

    forgot to make bool optional. With out the optional flag we dont really know if the field was left off or set to false.

--- powergrader_event_utils/events/proto_events/course_pb2.py
+++ powergrader_event_utils/events/proto_events/course_pb2.py
@@ -14,7 +14,7 @@ _sym_db = _symbol_database.Default()
 
 
 
-DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x0c\x63ourse.proto\x12\x06\x63ourse\"\x88\x01\n\x06\x43ourse\x12\x11\n\tpublic_id\x18\x01 \x01(\t\x12\n\n\x02id\x18\x02 \x01(\t\x12\x17\n\x0forganization_id\x18\x03 \x01(\t\x12\x15\n\rinstructor_id\x18\x04 \x01(\t\x12\x0c\n\x04name\x18\x05 \x01(\t\x12\x13\n\x0b\x64\x65scription\x18\x06 \x01(\t\x12\x0c\n\x04when\x18\x07 \x01(\x04\"j\n\x07Section\x12\x11\n\tpublic_id\x18\x01 \x01(\t\x12\n\n\x02id\x18\x02 \x01(\t\x12\x11\n\tcourse_id\x18\x03 \x01(\t\x12\x0c\n\x04name\x18\x04 \x01(\t\x12\x11\n\tis_active\x18\x05 \x01(\x08\x12\x0c\n\x04when\x18\x07 \x01(\x04\"6\n\x0cOrganization\x12\n\n\x02id\x18\x01 \x01(\t\x12\x0c\n\x04name\x18\x02 \x01(\t\x12\x0c\n\x04\x63ode\x18\x03 \x01(\tb\x06proto3')
+DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x0c\x63ourse.proto\x12\x06\x63ourse\"\x88\x01\n\x06\x43ourse\x12\x11\n\tpublic_id\x18\x01 \x01(\t\x12\n\n\x02id\x18\x02 \x01(\t\x12\x17\n\x0forganization_id\x18\x03 \x01(\t\x12\x15\n\rinstructor_id\x18\x04 \x01(\t\x12\x0c\n\x04name\x18\x05 \x01(\t\x12\x13\n\x0b\x64\x65scription\x18\x06 \x01(\t\x12\x0c\n\x04when\x18\x07 \x01(\x04\"}\n\x07Section\x12\x11\n\tpublic_id\x18\x01 \x01(\t\x12\n\n\x02id\x18\x02 \x01(\t\x12\x11\n\tcourse_id\x18\x03 \x01(\t\x12\x0c\n\x04name\x18\x04 \x01(\t\x12\x16\n\tis_active\x18\x05 \x01(\x08H\x00\x88\x01\x01\x12\x0c\n\x04when\x18\x07 \x01(\x04\x42\x0c\n\n_is_active\"6\n\x0cOrganization\x12\n\n\x02id\x18\x01 \x01(\t\x12\x0c\n\x04name\x18\x02 \x01(\t\x12\x0c\n\x04\x63ode\x18\x03 \x01(\tb\x06proto3')
 
 _globals = globals()
 _builder.BuildMessageAndEnumDescriptors(DESCRIPTOR, _globals)
@@ -24,7 +24,7 @@ if _descriptor._USE_C_DESCRIPTORS == False:
   _globals['_COURSE']._serialized_start=25
   _globals['_COURSE']._serialized_end=161
   _globals['_SECTION']._serialized_start=163
-  _globals['_SECTION']._serialized_end=269
-  _globals['_ORGANIZATION']._serialized_start=271
-  _globals['_ORGANIZATION']._serialized_end=325
+  _globals['_SECTION']._serialized_end=288
+  _globals['_ORGANIZATION']._serialized_start=290
+  _globals['_ORGANIZATION']._serialized_end=344
 # @@protoc_insertion_point(module_scope)
