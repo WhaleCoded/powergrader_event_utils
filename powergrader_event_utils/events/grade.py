3a578217fbc74bb111672a98e44ea741fd202699
HesitantlyHuman <tannersims@hesitantlyhuman.com>
2024-02-05 03:25:25 -0700

    Fix criterion vs criteria naming

--- powergrader_event_utils/events/grade.py
+++ powergrader_event_utils/events/grade.py
@@ -12,43 +12,43 @@ from powergrader_event_utils.events.utils import (
     general_proto_type_init,
 )
 from powergrader_event_utils.events.proto_events.assignment_pb2 import (
-    AICriteriaGradingStarted,
+    AICriterionGradingStarted,
     GradingMethod as GradingMethodProto,
     Grade as GradeProto,
-    AICriteriaGrade,
-    AIInferredCriteriaGrade,
-    InstructorCriteriaGrade,
-    InstructorOverrideCriteriaGrade,
-    CriteriaGradeEmbedding,
+    AICriterionGrade,
+    AIInferredCriterionGrade,
+    InstructorCriterionGrade,
+    InstructorOverrideCriterionGrade,
+    CriterionGradeEmbedding,
     InstructorSubmissionGradeApproval,
 )
 
 
 @dataclass
-class AICriteriaGradingStartedEvent(
-    PowerGraderEvent, ProtoWrapper[AICriteriaGradingStarted]
+class AICriterionGradingStartedEvent(
+    PowerGraderEvent, ProtoWrapper[AICriterionGradingStarted]
 ):
     version_uuid: str
-    criteria_uuid: str
+    criterion_uuid: str
     submission_version_uuid: str
     time_started: int
 
     def __init__(
         self,
-        criteria_uuid: str,
+        criterion_uuid: str,
         submission_version_uuid: str,
         time_started: int,
     ) -> None:
         general_proto_type_init(
             object_to_initialize=self,
-            proto_type=AICriteriaGradingStarted,
+            proto_type=AICriterionGradingStarted,
             id_field_to_initialize="version_uuid",
-            criteria_uuid=criteria_uuid,
+            criterion_uuid=criterion_uuid,
             submission_version_uuid=submission_version_uuid,
             time_started=time_started,
         )
 
-    def _package_into_proto(self) -> AICriteriaGradingStarted:
+    def _package_into_proto(self) -> AICriterionGradingStarted:
         return self.proto
 
     @staticmethod
@@ -56,9 +56,9 @@ class AICriteriaGradingStartedEvent(
         raise NotImplementedError
 
     @classmethod
-    def deserialize(cls, event: bytes) -> "AICriteriaGradingStartedEvent":
+    def deserialize(cls, event: bytes) -> "AICriterionGradingStartedEvent":
         return general_deserialization(
-            AICriteriaGradingStarted, cls, event, "version_uuid"
+            AICriterionGradingStarted, cls, event, "version_uuid"
         )
 
 
@@ -117,9 +117,9 @@ class Grade(ProtoWrapper[GradeProto]):
 
 
 @dataclass
-class AICriteriaGradeEvent(PowerGraderEvent, ProtoWrapper[AICriteriaGrade]):
+class AICriterionGradeEvent(PowerGraderEvent, ProtoWrapper[AICriterionGrade]):
     grading_started_version_uuid: str
-    criteria_uuid: str
+    criterion_uuid: str
     submission_version_uuid: str
     grading_method_uuid: str
     grade: Grade
@@ -128,7 +128,7 @@ class AICriteriaGradeEvent(PowerGraderEvent, ProtoWrapper[AICriteriaGrade]):
     def __init__(
         self,
         grading_started_version_uuid: str,
-        criteria_uuid: str,
+        criterion_uuid: str,
         submission_version_uuid: str,
         grading_method_uuid: str,
         grade: Grade,
@@ -136,17 +136,17 @@ class AICriteriaGradeEvent(PowerGraderEvent, ProtoWrapper[AICriteriaGrade]):
     ) -> None:
         general_proto_type_init(
             object_to_initialize=self,
-            proto_type=AICriteriaGrade,
+            proto_type=AICriterionGrade,
             id_field_to_initialize=None,
             grading_started_version_uuid=grading_started_version_uuid,
-            criteria_uuid=criteria_uuid,
+            criterion_uuid=criterion_uuid,
             submission_version_uuid=submission_version_uuid,
             grading_method_uuid=grading_method_uuid,
             grade=grade,
             time_finished=time_finished,
         )
 
-    def _package_into_proto(self) -> AICriteriaGrade:
+    def _package_into_proto(self) -> AICriterionGrade:
         return self.proto
 
     @staticmethod
@@ -154,51 +154,51 @@ class AICriteriaGradeEvent(PowerGraderEvent, ProtoWrapper[AICriteriaGrade]):
         raise NotImplementedError
 
     @classmethod
-    def deserialize(cls, event: bytes) -> "AICriteriaGradeEvent":
+    def deserialize(cls, event: bytes) -> "AICriterionGradeEvent":
         return general_deserialization(
-            AICriteriaGrade, cls, event, "grading_started_version_uuid"
+            AICriterionGrade, cls, event, "grading_started_version_uuid"
         )
 
 
 @dataclass
-class AIInferredCriteriaGradeEvent(
-    PowerGraderEvent, ProtoWrapper[AIInferredCriteriaGrade]
+class AIInferredCriterionGradeEvent(
+    PowerGraderEvent, ProtoWrapper[AIInferredCriterionGrade]
 ):
     grading_started_version_uuid: str
-    criteria_uuid: str
+    criterion_uuid: str
     submission_version_uuid: str
     grading_method_uuid: str
-    previous_criteria_grade_version_uuid: str
-    faculty_override_criteria_grade_version_uuid: str
+    previous_criterion_grade_version_uuid: str
+    faculty_override_criterion_grade_version_uuid: str
     grade: Grade
     time_finished: int
 
     def __init__(
         self,
         grading_started_version_uuid: str,
-        criteria_uuid: str,
+        criterion_uuid: str,
         submission_version_uuid: str,
         grading_method_uuid: str,
-        previous_criteria_grade_version_uuid: str,
-        faculty_override_criteria_grade_version_uuid: str,
+        previous_criterion_grade_version_uuid: str,
+        faculty_override_criterion_grade_version_uuid: str,
         grade: Grade,
         time_finished: int,
     ) -> None:
         general_proto_type_init(
             object_to_initialize=self,
-            proto_type=AIInferredCriteriaGrade,
+            proto_type=AIInferredCriterionGrade,
             id_field_to_initialize=None,
             grading_started_version_uuid=grading_started_version_uuid,
-            criteria_uuid=criteria_uuid,
+            criterion_uuid=criterion_uuid,
             submission_version_uuid=submission_version_uuid,
             grading_method_uuid=grading_method_uuid,
-            previous_criteria_grade_version_uuid=previous_criteria_grade_version_uuid,
-            faculty_override_criteria_grade_version_uuid=faculty_override_criteria_grade_version_uuid,
+            previous_criterion_grade_version_uuid=previous_criterion_grade_version_uuid,
+            faculty_override_criterion_grade_version_uuid=faculty_override_criterion_grade_version_uuid,
             grade=grade,
             time_finished=time_finished,
         )
 
-    def _package_into_proto(self) -> AIInferredCriteriaGrade:
+    def _package_into_proto(self) -> AIInferredCriterionGrade:
         return self.proto
 
     @staticmethod
@@ -206,40 +206,40 @@ class AIInferredCriteriaGradeEvent(
         raise NotImplementedError
 
     @classmethod
-    def deserialize(cls, event: bytes) -> "AIInferredCriteriaGradeEvent":
+    def deserialize(cls, event: bytes) -> "AIInferredCriterionGradeEvent":
         return general_deserialization(
-            AIInferredCriteriaGrade, cls, event, "grading_started_version_uuid"
+            AIInferredCriterionGrade, cls, event, "grading_started_version_uuid"
         )
 
 
 @dataclass
-class InstructorCriteriaGradeEvent(
-    PowerGraderEvent, ProtoWrapper[InstructorCriteriaGrade]
+class InstructorCriterionGradeEvent(
+    PowerGraderEvent, ProtoWrapper[InstructorCriterionGrade]
 ):
     version_uuid: str
-    criteria_uuid: str
+    criterion_uuid: str
     submission_version_uuid: str
     instructor_public_uuid: str
     grade: Grade
 
     def __init__(
         self,
-        criteria_uuid: str,
+        criterion_uuid: str,
         submission_version_uuid: str,
         instructor_public_uuid: str,
         grade: Grade,
     ) -> None:
         general_proto_type_init(
             object_to_initialize=self,
-            proto_type=InstructorCriteriaGrade,
+            proto_type=InstructorCriterionGrade,
             id_field_to_initialize="version_uuid",
-            criteria_uuid=criteria_uuid,
+            criterion_uuid=criterion_uuid,
             submission_version_uuid=submission_version_uuid,
             instructor_public_uuid=instructor_public_uuid,
             grade=grade,
         )
 
-    def _package_into_proto(self) -> InstructorCriteriaGrade:
+    def _package_into_proto(self) -> InstructorCriterionGrade:
         return self.proto
 
     @staticmethod
@@ -247,43 +247,43 @@ class InstructorCriteriaGradeEvent(
         raise NotImplementedError
 
     @classmethod
-    def deserialize(cls, event: bytes) -> "InstructorCriteriaGradeEvent":
+    def deserialize(cls, event: bytes) -> "InstructorCriterionGradeEvent":
         return general_deserialization(
-            InstructorCriteriaGrade, cls, event, "version_uuid"
+            InstructorCriterionGrade, cls, event, "version_uuid"
         )
 
 
 @dataclass
-class InstructorOverrideCriteriaGradeEvent(
-    PowerGraderEvent, ProtoWrapper[InstructorOverrideCriteriaGrade]
+class InstructorOverrideCriterionGradeEvent(
+    PowerGraderEvent, ProtoWrapper[InstructorOverrideCriterionGrade]
 ):
     version_uuid: str
-    criteria_uuid: str
+    criterion_uuid: str
     submission_version_uuid: str
-    previous_criteria_grade_version_uuid: str
+    previous_criterion_grade_version_uuid: str
     instructor_public_uuid: str
     grade: Grade
 
     def __init__(
         self,
-        criteria_uuid: str,
+        criterion_uuid: str,
         submission_version_uuid: str,
-        previous_criteria_grade_version_uuid: str,
+        previous_criterion_grade_version_uuid: str,
         instructor_public_uuid: str,
         grade: Grade,
     ) -> None:
         general_proto_type_init(
             object_to_initialize=self,
-            proto_type=InstructorOverrideCriteriaGrade,
+            proto_type=InstructorOverrideCriterionGrade,
             id_field_to_initialize="version_uuid",
-            criteria_uuid=criteria_uuid,
+            criterion_uuid=criterion_uuid,
             submission_version_uuid=submission_version_uuid,
-            previous_criteria_grade_version_uuid=previous_criteria_grade_version_uuid,
+            previous_criterion_grade_version_uuid=previous_criterion_grade_version_uuid,
             instructor_public_uuid=instructor_public_uuid,
             grade=grade,
         )
 
-    def _package_into_proto(self) -> InstructorOverrideCriteriaGrade:
+    def _package_into_proto(self) -> InstructorOverrideCriterionGrade:
         return self.proto
 
     @staticmethod
@@ -291,37 +291,37 @@ class InstructorOverrideCriteriaGradeEvent(
         raise NotImplementedError
 
     @classmethod
-    def deserialize(cls, event: bytes) -> "InstructorOverrideCriteriaGradeEvent":
+    def deserialize(cls, event: bytes) -> "InstructorOverrideCriterionGradeEvent":
         return general_deserialization(
-            InstructorOverrideCriteriaGrade, cls, event, "version_uuid"
+            InstructorOverrideCriterionGrade, cls, event, "version_uuid"
         )
 
 
 @dataclass
-class CriteriaGradeEmbeddingEvent(
-    PowerGraderEvent, ProtoWrapper[CriteriaGradeEmbedding]
+class CriterionGradeEmbeddingEvent(
+    PowerGraderEvent, ProtoWrapper[CriterionGradeEmbedding]
 ):
     version_uuid: str
-    criteria_grade_version_uuid: str
+    criterion_grade_version_uuid: str
     embedder_uuid: str
     embedding: List[float]
 
     def __init__(
         self,
-        criteria_grade_version_uuid: str,
+        criterion_grade_version_uuid: str,
         embedder_uuid: str,
         embedding: List[float],
     ) -> None:
         general_proto_type_init(
             object_to_initialize=self,
-            proto_type=CriteriaGradeEmbedding,
+            proto_type=CriterionGradeEmbedding,
             id_field_to_initialize="version_uuid",
-            criteria_grade_version_uuid=criteria_grade_version_uuid,
+            criterion_grade_version_uuid=criterion_grade_version_uuid,
             embedder_uuid=embedder_uuid,
             embedding=embedding,
         )
 
-    def _package_into_proto(self) -> CriteriaGradeEmbedding:
+    def _package_into_proto(self) -> CriterionGradeEmbedding:
         return self.proto
 
     @staticmethod
@@ -329,9 +329,9 @@ class CriteriaGradeEmbeddingEvent(
         raise NotImplementedError
 
     @classmethod
-    def deserialize(cls, event: bytes) -> "CriteriaGradeEmbeddingEvent":
+    def deserialize(cls, event: bytes) -> "CriterionGradeEmbeddingEvent":
         return general_deserialization(
-            CriteriaGradeEmbedding, cls, event, "version_uuid"
+            CriterionGradeEmbedding, cls, event, "version_uuid"
         )
 
 
@@ -342,14 +342,14 @@ class InstructorSubmissionGradeApprovalEvent(
     version_uuid: str
     submission_version_uuid: str
     instructor_public_uuid: str
-    criteria_grade_version_uuids: List[str]
+    criterion_grade_version_uuids: List[str]
     version_timestamp: int
 
     def __init__(
         self,
         submission_version_uuid: str,
         instructor_public_uuid: str,
-        criteria_grade_version_uuids: List[str],
+        criterion_grade_version_uuids: List[str],
         version_timestamp: int,
     ) -> None:
         general_proto_type_init(
@@ -358,7 +358,7 @@ class InstructorSubmissionGradeApprovalEvent(
             id_field_to_initialize="version_uuid",
             submission_version_uuid=submission_version_uuid,
             instructor_public_uuid=instructor_public_uuid,
-            criteria_grade_version_uuids=criteria_grade_version_uuids,
+            criterion_grade_version_uuids=criterion_grade_version_uuids,
             version_timestamp=version_timestamp,
         )
 
@@ -377,14 +377,14 @@ class InstructorSubmissionGradeApprovalEvent(
 
 
 if __name__ == "__main__":
-    ai_criterion_grading_started = AICriteriaGradingStartedEvent(
-        criteria_uuid="123",
+    ai_criterion_grading_started = AICriterionGradingStartedEvent(
+        criterion_uuid="123",
         submission_version_uuid="123",
         time_started=123,
     )
     print(ai_criterion_grading_started.serialize())
     print(
-        AICriteriaGradingStartedEvent.deserialize(
+        AICriterionGradingStartedEvent.deserialize(
             ai_criterion_grading_started.serialize()
         )
     )
@@ -402,71 +402,75 @@ if __name__ == "__main__":
         assessment="test",
     )
 
-    ai_criterion_grade = AICriteriaGradeEvent(
+    ai_criterion_grade = AICriterionGradeEvent(
         grading_started_version_uuid="123",
-        criteria_uuid="123",
+        criterion_uuid="123",
         submission_version_uuid="123",
         grading_method_uuid="123",
         grade=grade,
         time_finished=123,
     )
     print(ai_criterion_grade.serialize())
-    print(AICriteriaGradeEvent.deserialize(ai_criterion_grade.serialize()))
+    print(AICriterionGradeEvent.deserialize(ai_criterion_grade.serialize()))
 
-    ai_inferred_criterion_grade = AIInferredCriteriaGradeEvent(
+    ai_inferred_criterion_grade = AIInferredCriterionGradeEvent(
         grading_started_version_uuid="123",
-        criteria_uuid="123",
+        criterion_uuid="123",
         submission_version_uuid="123",
         grading_method_uuid="123",
-        previous_criteria_grade_version_uuid="123",
-        faculty_override_criteria_grade_version_uuid="123",
+        previous_criterion_grade_version_uuid="123",
+        faculty_override_criterion_grade_version_uuid="123",
         grade=grade,
         time_finished=123,
     )
     print(ai_inferred_criterion_grade.serialize())
     print(
-        AIInferredCriteriaGradeEvent.deserialize(
+        AIInferredCriterionGradeEvent.deserialize(
             ai_inferred_criterion_grade.serialize()
         )
     )
 
-    instructor_criterion_grade = InstructorCriteriaGradeEvent(
-        criteria_uuid="123",
+    instructor_criterion_grade = InstructorCriterionGradeEvent(
+        criterion_uuid="123",
         submission_version_uuid="123",
         instructor_public_uuid="123",
         grade=grade,
     )
     print(instructor_criterion_grade.serialize())
     print(
-        InstructorCriteriaGradeEvent.deserialize(instructor_criterion_grade.serialize())
+        InstructorCriterionGradeEvent.deserialize(
+            instructor_criterion_grade.serialize()
+        )
     )
 
-    instructor_override_criterion_grade = InstructorOverrideCriteriaGradeEvent(
-        criteria_uuid="123",
+    instructor_override_criterion_grade = InstructorOverrideCriterionGradeEvent(
+        criterion_uuid="123",
         submission_version_uuid="123",
-        previous_criteria_grade_version_uuid="123",
+        previous_criterion_grade_version_uuid="123",
         instructor_public_uuid="123",
         grade=grade,
     )
     print(instructor_override_criterion_grade.serialize())
     print(
-        InstructorOverrideCriteriaGradeEvent.deserialize(
+        InstructorOverrideCriterionGradeEvent.deserialize(
             instructor_override_criterion_grade.serialize()
         )
     )
 
-    criteria_grade_embedding = CriteriaGradeEmbeddingEvent(
-        criteria_grade_version_uuid="123",
+    criterion_grade_embedding = CriterionGradeEmbeddingEvent(
+        criterion_grade_version_uuid="123",
         embedder_uuid="123",
         embedding=[1.0],
     )
-    print(criteria_grade_embedding.serialize())
-    print(CriteriaGradeEmbeddingEvent.deserialize(criteria_grade_embedding.serialize()))
+    print(criterion_grade_embedding.serialize())
+    print(
+        CriterionGradeEmbeddingEvent.deserialize(criterion_grade_embedding.serialize())
+    )
 
     instructor_submission_grade_approval = InstructorSubmissionGradeApprovalEvent(
         submission_version_uuid="123",
         instructor_public_uuid="123",
-        criteria_grade_version_uuids=["123"],
+        criterion_grade_version_uuids=["123"],
         version_timestamp=123,
     )
     print(instructor_submission_grade_approval.serialize())
